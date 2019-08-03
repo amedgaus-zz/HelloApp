e38c7d14d930a07b094f9a837ae6459cd20f401b
Anton Medgaus <medgaus@gmail.com>
2019-08-03 17:10:17 +0600

    Updating Hello message

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -10,7 +10,7 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Dana");
+            Console.WriteLine("Hello Dana, its your husband");
             Console.ReadKey();
         }
     }
