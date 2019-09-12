1292dfc7853e9e01cf7ac1e2910244e4c6322723
Anton Medgaus <anton_medgaus@epam.com>
2019-09-12 16:20:54 +0600

    Adding new functions

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloApp
 {
+    public static class SquareGenerator
+    {
+        public static IEnumerable<int> Generate(int maxStep)
+        {
+            for (int i = 1; i <= maxStep; i++)
+                yield return i * i;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Dana, its your husband");
-            Console.ReadKey();
             Byte a = 1;
             Byte b = (Byte)(a - 1);
-
+            foreach (var sq in SquareGenerator.Generate(5))
+                Console.WriteLine(sq);
+            Console.WriteLine(Char.IsUpper('א'));
+            Console.WriteLine(Char.IsLower('א'));
+            Console.ReadKey();
         }
     }
 }
